9ea670d1f6b264b3eb2cb328c43426b57e24069c
akshhh51 <akshaybutle9@gmail.com>
2021-11-24 18:35:15 +0530

    update struct.h

---
+++ 3_Implementation/inc/struct.h
@@ -0,0 +1,34 @@
+#include <stdio.h>  
+#include <conio.h>  
+#include <math.h>  
+#include <stdlib.h> 
+switch (op)
+{
+    case 1:
+    addition();
+    break;
+    
+    case 2:  
+    subtract();
+    break; 
+              
+    case 3:  
+    multiply(); 
+    break; //   
+              
+    case 4:  
+    divide();   
+    break; 
+
+    case 5:  
+    sq(); 
+    break; 
+
+    case 6:  
+    sqrt1(); 
+    break; 
+              
+    case 7:  
+    exit(0); 
+    break;
+}
