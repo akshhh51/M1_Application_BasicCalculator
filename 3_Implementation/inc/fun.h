046dcb8ac79b1bd14c320149bd8933a3af7757ba
akshhh51 <akshaybutle9@gmail.com>
2021-11-24 17:12:30 +0530

    update inc

---
+++ 3_Implementation/inc/fun.h
@@ -0,0 +1,21 @@
+/**
+ * @file fun.h
+ * @author akshhh51
+ * @brief functions
+ * @version 0.1
+ * @date 2021-11-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ * 
+ */
+#ifndef _FUN_H
+#define _FUN_H
+int addition();  
+int subtract();  
+int multiply();  
+int divide();  
+int sq();  
+int sqrt1();  
+void exit(); 
+#endif
